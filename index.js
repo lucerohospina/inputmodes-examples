a76683fa93fa5aa51f79ffd1dc1b5953824dac0a
Lucero Hospina <lucero.hospina@globant.com>
2024-05-20 13:09:17 -0500

    more examples

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hi!');
+function toggleInputMode() {
+  console.log('toggleInputMode');
+  const inputField = document.getElementById('customInput');
+  // Toggle between numeric and text input modes
+  if (inputField.getAttribute('inputmode') === 'numeric') {
+    inputField.setAttribute('inputmode', 'text');
+  } else {
+    inputField.setAttribute('inputmode', 'numeric');
+  }
+}
