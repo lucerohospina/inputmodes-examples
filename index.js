a713f2bc01a91f1b4d3f8dfae9fe1bfde24e9a6e
Lucero Hospina <lucero.hospina@globant.com>
2024-05-20 14:37:37 -0500

    extra example

--- index.js
+++ index.js
@@ -8,3 +8,21 @@ function toggleInputMode() {
     inputField.setAttribute('inputmode', 'numeric');
   }
 }
+
+document.getElementById('alphanumericInput').addEventListener('input', function (e) {
+  var value = e.target.value;
+  var lastChar = value.charAt(value.length - 1);
+
+  // Check if the last character is a letter
+  if (/[a-zA-Z]/.test(lastChar)) {
+    // Temporarily remove focus to trigger input mode change
+    e.target.blur();
+    e.target.setAttribute('inputmode', 'text');
+    e.target.focus();
+  } else if (/\d/.test(lastChar)) {
+    // Switch back to numeric if last character is a number
+    e.target.blur();
+    e.target.setAttribute('inputmode', 'numeric');
+    e.target.focus();
+  }
+});
